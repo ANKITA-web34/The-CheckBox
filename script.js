616a5cbf2f1159f910f02a5f69421e1bf1addc58
Ankita <anki333soul@gmail.com>
2021-08-07 11:48:30 -0700

    add functionality

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const chechBoxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-console.log(chechBoxes);
+let lastChecked;
+
+function handleCheck(e) {
+    //if there are shift down & they are checked 
+    let inBetween = false;
+    if(e.shiftKey && this.checked) {
+        //loop over every single checkbox
+        checkBoxes.forEach(checkBox => {
+            if(checkBox === this || checkBox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            
+            if(inBetween) {
+                checkBox.checked = true;
+            }
+        });
+    }
+    
+    lastChecked = this;
+}
+
+
+checkBoxes.forEach(checkBox => checkBox.addEventListener('click', handleCheck));
